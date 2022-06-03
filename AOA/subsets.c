5957a283da263b6ae10ad5679c5105f37704649d
Sanika <sanikaardekar@gmail.com>
2022-06-03 22:44:41 +0530

    sum of subset corrected

--- AOA/subsets.c
+++ AOA/subsets.c
@@ -1,29 +1,40 @@
-#include <stdio.h>
-int w[]={2,3,4,5}, m=7, x[]={0,0,0,0};
+#include<stdio.h>
+int counter=1;//new
+int n=4;
+int w[4]={2,3,4,5};
+int x[4]={0,0,0,0};
+int m=7;
+int total=0;
 
-void sumofsubsets(int s, int k, int r)
-{
-    x[k] = 1;
-    if(s+w[k]==m)
-    {
-        for(int t=0;t<k;t++)
-        {
-            printf("%d ",x[t]);
-        }
-    }
-    else if(s+w[k]+w[k+1]<=m)
-    {
-        sumofsubsets(s+w[k],k+1,r-w[k]);
-    }
-    if((s+r-w[k]>=m) && (s+w[k+1]<=m))
-    {
-        x[k] = 0;
-        sumofsubsets(s,k+1,r-w[k]);
-    }
+void sumOfSubset(int s, int k, int r){
+	int j;//new
+	x[k]=1;
+	if(s+w[k]==m){
+		printf("Solution %d: ",counter);//new
+		counter++;//new
+		for(j=0;j<n;j++){//condition till number of digits
+			printf(" %d ",x[j]);				
+		}
+		printf("\n");//new
+		for(j=0;j<n;j++){
+			x[j]=0;//new
+		}						
+	}
+	else if(k!=n-1 && s+w[k]+w[k+1]<=m){//new
+		sumOfSubset(s+w[k],k+1,r-w[k] );	
+	}
+	if(s+r-w[k]>=m && s+w[k+1]<=m && k!=n-1 ){//new
+		x[k]=0;
+		sumOfSubset(s,k+1,r-w[k]);	
+	}
+	
 }
-int main()
-{
-   sumofsubsets(0,0,14);
 
-    return 0;
+int main(){
+	int i;
+	for(i=0;i<n;i++){
+		total+=w[i];
+	}
+	sumOfSubset(0,0,total);
+	return 0;
 }
