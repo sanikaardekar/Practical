d8d21bb958bbcc0c43496608a747072a282a8c10
Sanika <sanikaardekar@gmail.com>
2022-06-07 02:27:04 +0530

    corrected kmp n and m variables

--- AOA/kmp.c
+++ AOA/kmp.c
@@ -3,26 +3,26 @@
 #include <stdlib.h>
  
 // Function to implement the KMP algorithm
-void KMP(const char* text, const char* pattern, int m, int n)
+void KMP(const char* text, const char* pattern, int n, int m)
 {
     // base case 1: pattern is NULL or empty
-    if (*pattern == '\0' || n == 0) {
+    if (*pattern == '\0' || m == 0) {
         printf("The pattern occurs with shift 0");
     }
  
     // base case 2: text is NULL, or text's length is less than that of pattern's
-    if (*text == '\0' || n > m) {
+    if (*text == '\0' || m > n) {
         printf("Pattern not found");
     }
  
     // next[i] stores the index of the next best partial match
-    int next[n + 1];
+    int next[m + 1];
  
-    for (int i = 0; i < n + 1; i++) {
+    for (int i = 0; i < m + 1; i++) {
         next[i] = 0;
     }
  
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < m; i++)
     {
         int j = next[i + 1];
  
@@ -35,11 +35,11 @@ void KMP(const char* text, const char* pattern, int m, int n)
         }
     }
  
-    for (int i = 0, j = 0; i < m; i++)
+    for (int i = 0, j = 0; i < n; i++)
     {
         if (*(text + i) == *(pattern + j))
         {
-            if (++j == n) {
+            if (++j == m) {
                 printf("The pattern occurs at position %d\n", i - j + 2);
             }
         }
